c3cb7b3c4717e0013178da25283fba24764e368b
Aniket Mishra <aniketmishrarr@gmail.com>
2026-02-25 14:29:24 +0530

    [Aniket] Added: feat [uc2] : accept word inputs

--- src/main/java/com/lexicaltwist/LexicalTwistPuzzle.java
+++ src/main/java/com/lexicaltwist/LexicalTwistPuzzle.java
@@ -1,7 +1,15 @@
 package com.lexicaltwist;
+import java.util.Scanner;
 
 public class LexicalTwistPuzzle {
     public static void main(String[] args) {
-        System.out.println("Lexical Twist Puzzle");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first word: ");
+        String word1 = scanner.next();
+        System.out.print("Enter second word: ");
+        String word2 = scanner.next();
+        System.out.println("Word 1: " + word1);
+        System.out.println("Word 2: " + word2);
+        scanner.close();
     }
 }
